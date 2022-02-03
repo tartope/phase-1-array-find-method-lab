7941c6e497cfc90303d6d2e37dbc047c441969bc
Tunisia Artope <artope@yahoo.com>
2022-02-03 13:24:52 -0500

    commit tree 3a7f08899e1eb458fdb0d3b8e6765a33ba77a382

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+
+function superbowlWin(objects) {
+    // first find if we have the object 
+    let foundObject = objects.find(item => (item.result === "W"));
+
+    // test if we found
+    if (foundObject) {
+        return foundObject.year;
+    } else {
+        return undefined;
+    }
+}
+
+
+// function superbowlWin(records){
+//     const result = records.find(record => record.result === 'W');
+//     if(result === 'W') {
+//         return year
+//     }
+// return
+// };
